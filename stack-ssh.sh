d1f47a03e2d4a620b7a82fd2d5a560dd633388db
Assaf Lavie <me@assaflavie.com>
2015-03-19 09:47:22 +0200

    Renamed script (no .py extension) and added a wrapper script to populate .ssh/config

---
+++ stack-ssh.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export tgt=`stat -f "%N" ~/.ssh/config`
+if [ -e "$tgt" ]; then
+
+    export backup="${tgt}_backup_`date +%s`"
+    echo "backing up $tgt to $backup"
+    cp $tgt $backup
+fi
+
+
+echo "Populating $tgt with AWS EC2 instances"
+./aws-ssh-config --tags "aws:cloudformation:stack-name,Name" | tee "$tgt"
+
+echo "Done."
+
