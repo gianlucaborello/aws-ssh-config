710e64d384fb3e092250abe8f7116322f4996180
Gianluca Borello <g.borello@gmail.com>
2016-05-07 15:52:40 -0700

    Ignore instances without key. Fixes #12

--- aws-ssh-config.py
+++ aws-ssh-config.py
@@ -5,132 +5,139 @@ import boto.ec2
 
 
 AMIS_TO_USER = {
-	'amzn' : 'ec2-user',
-	'ubuntu' : 'ubuntu',
-	'CentOS' : 'root',
-	'DataStax' : 'ubuntu',
-	'CoreOS' : 'core'
+    'amzn' : 'ec2-user',
+    'ubuntu' : 'ubuntu',
+    'CentOS' : 'root',
+    'DataStax' : 'ubuntu',
+    'CoreOS' : 'core'
 }
 
 BLACKLISTED_REGIONS = [
-	'cn-north-1',
-	'us-gov-west-1'
+    'cn-north-1',
+    'us-gov-west-1'
 ]
 
 
 def generate_id(instance, tags_filter, region):
-	id = ''
+    id = ''
 
-	if tags_filter is not None:
-		for tag in tags_filter.split(','):
-			value = instance.tags.get(tag, None)
-			if value:
-				if not id:
-					id = value
-				else:
-					id += '-' + value
-	else:
-		for tag, value in instance.tags.iteritems():
-			if not tag.startswith('aws'):
-				if not id:
-					id = value
-				else:
-					id += '-' + value
+    if tags_filter is not None:
+        for tag in tags_filter.split(','):
+            value = instance.tags.get(tag, None)
+            if value:
+                if not id:
+                    id = value
+                else:
+                    id += '-' + value
+    else:
+        for tag, value in instance.tags.iteritems():
+            if not tag.startswith('aws'):
+                if not id:
+                    id = value
+                else:
+                    id += '-' + value
 
-	if not id:
-		id = instance.id
+    if not id:
+        id = instance.id
 
-	if region:
-		id += '-' + instance.placement
+    if region:
+        id += '-' + instance.placement
 
-	return id
+    return id
 
 
 def main():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--tags', help='A comma-separated list of tag names to be considered for concatenation. If omitted, all tags will be used')
-	parser.add_argument('--region', action='store_true', help='Append the region name at the end of the concatenation')
-	parser.add_argument('--private', action='store_true', help='Use private IP addresses (public are used by default)')
-	parser.add_argument('--profile', help='specify aws credential profile to use')
-	parser.add_argument('--user', help='override the ssh username for all hosts')
-	parser.add_argument('--default-user', help='default ssh username to use if we cannot detect from AMI name')
-	parser.add_argument('--prefix', default='', help='specify a prefix to prepend to all host names')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--tags', help='A comma-separated list of tag names to be considered for concatenation. If omitted, all tags will be used')
+    parser.add_argument('--region', action='store_true', help='Append the region name at the end of the concatenation')
+    parser.add_argument('--private', action='store_true', help='Use private IP addresses (public are used by default)')
+    parser.add_argument('--profile', help='specify aws credential profile to use')
+    parser.add_argument('--user', help='override the ssh username for all hosts')
+    parser.add_argument('--default-user', help='default ssh username to use if we cannot detect from AMI name')
+    parser.add_argument('--prefix', default='', help='specify a prefix to prepend to all host names')
 
-	args = parser.parse_args()
+    args = parser.parse_args()
 
-	instances = {}
-	counts_total = {}
-	counts_incremental = {}
-	amis = {}
+    instances = {}
+    counts_total = {}
+    counts_incremental = {}
+    amis = {}
 
-	for region in boto.ec2.regions():
-		if region.name in BLACKLISTED_REGIONS:
-			continue
-		if args.profile:
-			conn = boto.ec2.connect_to_region(region.name,profile_name=args.profile)
-		else:
-			conn = boto.ec2.connect_to_region(region.name)
+    for region in boto.ec2.regions():
+        if region.name in BLACKLISTED_REGIONS:
+            continue
+        if args.profile:
+            conn = boto.ec2.connect_to_region(region.name,profile_name=args.profile)
+        else:
+            conn = boto.ec2.connect_to_region(region.name)
 
-		for instance in conn.get_only_instances():
-			if instance.state == 'running' and not instance.platform == 'windows':
-				if instance.launch_time not in instances:
-					instances[instance.launch_time] = []
+        for instance in conn.get_only_instances():
+            if instance.state != 'running':
+                continue
 
-				instances[instance.launch_time].append(instance)
+            if instance.platform == 'windows':
+                continue
 
-				id = generate_id(instance, args.tags, args.region)
+            if instance.key_name is None:
+                continue
 
-				if id not in counts_total:
-					counts_total[id] = 0
-					counts_incremental[id] = 0
+            if instance.launch_time not in instances:
+                instances[instance.launch_time] = []
 
-				counts_total[id] += 1
+            instances[instance.launch_time].append(instance)
 
-				if args.user:
-					amis[instance.image_id] = args.user
-				else:
-					if not instance.image_id in amis:
-						image = conn.get_image(instance.image_id)
+            id = generate_id(instance, args.tags, args.region)
 
-						for ami, user in AMIS_TO_USER.iteritems():
-							regexp = re.compile(ami)
-							if image and regexp.match(image.name):
-								amis[instance.image_id] = user
-								break
+            if id not in counts_total:
+                counts_total[id] = 0
+                counts_incremental[id] = 0
 
-						if image and instance.image_id not in amis:
-							amis[instance.image_id] = args.default_user
-							if args.default_user is None:
-								sys.stderr.write('Can\'t lookup user for AMI \'' + image.name + '\', add a rule to the script\n')
+            counts_total[id] += 1
 
+            if args.user:
+                amis[instance.image_id] = args.user
+            else:
+                if not instance.image_id in amis:
+                    image = conn.get_image(instance.image_id)
 
-	for k in sorted(instances):
-		for instance in instances[k]:
-			if args.private:
-				if instance.private_ip_address:
-					ip = instance.private_ip_address
-			else:
-				if instance.ip_address:
-					ip = instance.ip_address
+                    for ami, user in AMIS_TO_USER.iteritems():
+                        regexp = re.compile(ami)
+                        if image and regexp.match(image.name):
+                            amis[instance.image_id] = user
+                            break
 
-			id = generate_id(instance, args.tags, args.region)
+                    if image and instance.image_id not in amis:
+                        amis[instance.image_id] = args.default_user
+                        if args.default_user is None:
+                            sys.stderr.write('Can\'t lookup user for AMI \'' + image.name + '\', add a rule to the script\n')
 
-			if counts_total[id] != 1:
-				counts_incremental[id] += 1
-				id += '-' + str(counts_incremental[id])
+    for k in sorted(instances):
+        for instance in instances[k]:
+            if args.private:
+                if instance.private_ip_address:
+                    ip = instance.private_ip_address
+            else:
+                if instance.ip_address:
+                    ip = instance.ip_address
 
-			print 'Host ' + args.prefix + id
-			print '    HostName ' + ip
+            id = generate_id(instance, args.tags, args.region)
 
-			try:
-				if amis[instance.image_id] is not None:
-					print '    User ' + amis[instance.image_id]
-			except:
-				pass
+            if counts_total[id] != 1:
+                counts_incremental[id] += 1
+                id += '-' + str(counts_incremental[id])
 
-			print '    IdentityFile ~/.ssh/' + instance.key_name + '.pem'
-			print '    StrictHostKeyChecking no' # just for me, removing this is usually a good choice
-			print
+            print 'Host ' + args.prefix + id
+            print '    HostName ' + ip
+
+            try:
+                if amis[instance.image_id] is not None:
+                    print '    User ' + amis[instance.image_id]
+            except:
+                pass
+
+            print '    IdentityFile ~/.ssh/' + instance.key_name + '.pem'
+            print '    StrictHostKeyChecking no' # just for me, removing this is usually a good choice
+            print
 
 
 if __name__ == '__main__':
