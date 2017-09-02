654d54e17205a890e7b5ced7e8b384ec6988f27d
ltagliamonte <luigi.tagliamonte86@gmail.com>
2017-09-01 17:31:38 -0700

    allow to override the key to use

--- aws-ssh-config.py
+++ aws-ssh-config.py
@@ -58,6 +58,7 @@ def main():
     parser.add_argument('--private', action='store_true', help='Use private IP addresses (public are used by default)')
     parser.add_argument('--profile', help='Specify AWS credential profile to use')
     parser.add_argument('--region', action='store_true', help='Append the region name at the end of the concatenation')
+    parser.add_argument('--ssh-key-name', default='', help='Override the ssh key to use')
     parser.add_argument('--strict-hostkey-checking', action='store_true', help='Do not include StrictHostKeyChecking=no in ssh config')
     parser.add_argument('--tags', help='A comma-separated list of tag names to be considered for concatenation. If omitted, all tags will be used')
     parser.add_argument('--user', help='Override the ssh username for all hosts')
@@ -163,7 +164,11 @@ def main():
             else:
                 keydir = '~/.ssh/'
 
-            print '    IdentityFile ' + keydir + instance.key_name.replace(' ', '_') + '.pem'
+            if args.ssh_key_name:
+                print '    IdentityFile ~/.ssh/' + keydir + args.ssh_key_name + '.pem'
+            else:
+                print '    IdentityFile ' + keydir + instance.key_name.replace(' ', '_') + '.pem'
+
             if not args.no_identities_only:
                 # ensure ssh-agent keys don't flood when we know the right file to use
                 print '    IdentitiesOnly yes'
