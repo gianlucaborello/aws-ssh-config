f6f669f97a566d4d588e63474cfe6e67b1576c9b
Michael Leer <michael.leer@mention-me.com>
2019-03-20 23:26:28 +0000

    pep8 / pycodestyle compliant

--- aws-ssh-config.py
+++ aws-ssh-config.py
@@ -8,24 +8,25 @@ import boto3
 
 
 AMI_NAMES_TO_USER = {
-    'amzn' : 'ec2-user',
-    'ubuntu' : 'ubuntu',
-    'CentOS' : 'root',
-    'DataStax' : 'ubuntu',
-    'CoreOS' : 'core'
+    'amzn': 'ec2-user',
+    'ubuntu': 'ubuntu',
+    'CentOS': 'root',
+    'DataStax': 'ubuntu',
+    'CoreOS': 'core'
 }
 
 AMI_IDS_TO_USER = {
-    'ami-ada2b6c4' : 'ubuntu'
+    'ami-ada2b6c4': 'ubuntu'
 }
 
 AMI_IDS_TO_KEY = {
-    'ami-ada2b6c4' : 'custom_key'
+    'ami-ada2b6c4': 'custom_key'
 }
 
 BLACKLISTED_REGIONS = [
 
 ]
+
 
 def generate_id(instance, tags_filter, region):
     instance_id = ''
@@ -51,27 +52,71 @@ def generate_id(instance, tags_filter, region):
         instance_id = instance['Instances'][0]['InstanceId']
 
     if region:
-        instance_id += '-' + instance['Instances'][0]['Placement']['AvailabilityZone']
+        instance_id += '-' + instance[
+            'Instances'][0]['Placement']['AvailabilityZone']
 
     return instance_id
 
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--default-user', help='Default ssh username to use if it can\'t be detected from AMI name')
-    parser.add_argument('--keydir', default='~/.ssh/', help='Location of private keys')
-    parser.add_argument('--no-identities-only', action='store_true', help='Do not include IdentitiesOnly=yes in ssh config; may cause connection refused if using ssh-agent')
-    parser.add_argument('--postfix', default='', help='Specify a postfix to append to all host names')
-    parser.add_argument('--prefix', default='', help='Specify a prefix to prepend to all host names')
-    parser.add_argument('--private', action='store_true', help='Use private IP addresses (public are used by default)')
-    parser.add_argument('--profile', help='Specify AWS credential profile to use')
-    parser.add_argument('--proxy', default='', help='Specify a bastion host for ProxyCommand')
-    parser.add_argument('--region', action='store_true', help='Append the region name at the end of the concatenation')
-    parser.add_argument('--ssh-key-name', default='', help='Override the ssh key to use')
-    parser.add_argument('--strict-hostkey-checking', action='store_true', help='Do not include StrictHostKeyChecking=no in ssh config')
-    parser.add_argument('--tags', help='A comma-separated list of tag names to be considered for concatenation. If omitted, all tags will be used')
-    parser.add_argument('--user', help='Override the ssh username for all hosts')
-    parser.add_argument('--white-list-region', default='', help='Which regions must be included. If omitted, all regions are considered', nargs='+')
-
+    parser.add_argument(
+        '--default-user',
+        help='Default ssh username to use'
+             'if it can\'t be detected from AMI name')
+    parser.add_argument(
+        '--keydir',
+        default='~/.ssh/',
+        help='Location of private keys')
+    parser.add_argument(
+        '--no-identities-only',
+        action='store_true',
+        help='Do not include IdentitiesOnly=yes in ssh config; may cause'
+             ' connection refused if using ssh-agent')
+    parser.add_argument(
+        '--postfix',
+        default='',
+        help='Specify a postfix to append to all host names')
+    parser.add_argument(
+        '--prefix',
+        default='',
+        help='Specify a prefix to prepend to all host names')
+    parser.add_argument(
+        '--private',
+        action='store_true',
+        help='Use private IP addresses (public are used by default)')
+    parser.add_argument(
+        '--profile',
+        help='Specify AWS credential profile to use')
+    parser.add_argument(
+        '--proxy',
+        default='',
+        help='Specify a bastion host for ProxyCommand')
+    parser.add_argument(
+        '--region',
+        action='store_true',
+        help='Append the region name at the end of the concatenation')
+    parser.add_argument(
+        '--ssh-key-name',
+        default='',
+        help='Override the ssh key to use')
+    parser.add_argument(
+        '--strict-hostkey-checking',
+        action='store_true',
+        help='Do not include StrictHostKeyChecking=no in ssh config')
+    parser.add_argument(
+        '--tags',
+        help='A comma-separated list of tag names to be considered for'
+             ' concatenation. If omitted, all tags will be used')
+    parser.add_argument(
+        '--user',
+        help='Override the ssh username for all hosts')
+    parser.add_argument(
+        '--white-list-region',
+        default='',
+        help='Which regions must be included. If omitted, all regions'
+             ' are considered',
+        nargs='+')
     args = parser.parse_args()
 
     instances = {}
@@ -89,7 +134,8 @@ def main():
     else:
         regions = boto3.client('ec2').describe_regions()['Regions']
     for region in regions:
-        if args.white_list_region and region['RegionName'] not in args.white_list_region:
+        if (args.white_list_region
+                and region['RegionName'] not in args.white_list_region):
             continue
         if region['RegionName'] in BLACKLISTED_REGIONS:
             continue
@@ -122,19 +168,37 @@ def main():
                 amis[instance['Instances'][0]['ImageId']] = args.user
             else:
                 if not instance['Instances'][0]['ImageId'] in amis:
-                    image = conn.describe_images(Filters=[{'Name':'image-id','Values':[instance['Instances'][0]['ImageId']]}])
+                    image = conn.describe_images(
+                        Filters=[
+                            {
+                                'Name': 'image-id',
+                                'Values': [instance['Instances'][0]['ImageId']]
+                            }
+                        ]
+                    )
 
                     for ami, user in AMI_NAMES_TO_USER.items():
                         regexp = re.compile(ami)
-                        if len(image['Images']) > 0 and regexp.match(image['Images'][0]['Name']):
+                        if (len(image['Images']) > 0
+                                and regexp.match(image['Images'][0]['Name'])):
                             amis[instance['Instances'][0]['ImageId']] = user
                             break
 
                     if instance['Instances'][0]['ImageId'] not in amis:
-                        amis[instance['Instances'][0]['ImageId']] = args.default_user
+                        amis[
+                            instance['Instances'][0]['ImageId']
+                        ] = args.default_user
                         if args.default_user is None:
-                            image_label = image['Images'][0]['ImageId'] if image['Images'][0] is not None else instance['Instances'][0]['ImageId']
-                            sys.stderr.write('Can\'t lookup user for AMI \'' + image_label + '\', add a rule to the script\n')
+                            image_label = image[
+                                'Images'
+                            ][0][
+                                'ImageId'] if image[
+                                    'Images'][0] is not None else instance[
+                                        'Instances'][0]['ImageId']
+                            sys.stderr.write(
+                                'Can\'t lookup user for AMI \''
+                                + image_label + '\', add a rule to '
+                                'the script\n')
 
     for k in sorted(instances):
         for instance in instances[k]:
@@ -148,9 +212,11 @@ def main():
                     try:
                         ip_addr = instance['Instances'][0]['PrivateIpAddress']
                     except KeyError:
-                        sys.stderr.write('Cannot lookup ip address for instance %s, skipped it.' % instance['Instances'][0]['InstanceId'])
+                        sys.stderr.write(
+                            'Cannot lookup ip address for instance %s,'
+                            ' skipped it.'
+                            % instance['Instances'][0]['InstanceId'])
                         continue
-
 
             instance_id = generate_id(instance, args.tags, args.region)
 
@@ -159,7 +225,7 @@ def main():
                 instance_id += '-' + str(counts_incremental[instance_id])
 
             hostid = args.prefix + instance_id + args.postfix
-            hostid = hostid.replace(' ', '_') # get rid of spaces
+            hostid = hostid.replace(' ', '_')  # get rid of spaces
 
             if instance['Instances'][0]['InstanceId']:
                 print('# id: ' + instance['Instances'][0]['InstanceId'])
@@ -175,14 +241,19 @@ def main():
                 keydir = '~/.ssh/'
 
             if args.ssh_key_name:
-                print('    IdentityFile ' + keydir + args.ssh_key_name + '.pem')
+                print('    IdentityFile '
+                      + keydir + args.ssh_key_name + '.pem')
             else:
-                key_name = AMI_IDS_TO_KEY.get(instance['Instances'][0]['ImageId'], instance['Instances'][0]['KeyName'])
+                key_name = AMI_IDS_TO_KEY.get(
+                    instance['Instances'][0]['ImageId'],
+                    instance['Instances'][0]['KeyName'])
 
-                print('    IdentityFile ' + keydir + key_name.replace(' ', '_') + '.pem')
+                print('    IdentityFile '
+                      + keydir + key_name.replace(' ', '_') + '.pem')
 
             if not args.no_identities_only:
-                # ensure ssh-agent keys don't flood when we know the right file to use
+                # ensure ssh-agent keys don't flood
+                # when we know the right file to use
                 print('    IdentitiesOnly yes')
             if not args.strict_hostkey_checking:
                 print('    StrictHostKeyChecking no')
